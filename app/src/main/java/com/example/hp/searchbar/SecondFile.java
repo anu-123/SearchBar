6e10c3995abd52c86dbaa65a54c8c37ce64055c1
anu-123 <r.annu492@gmail.com>
2017-03-16 17:13:41 +0530

    function added

--- app/src/main/java/com/example/hp/searchbar/SecondFile.java
+++ app/src/main/java/com/example/hp/searchbar/SecondFile.java
@@ -4,8 +4,9 @@ package com.example.hp.searchbar;
  * Created by HP on 3/16/2017.
  */
 public class SecondFile {
-    public void myFunction()
-    {
-        int i=10;
+    public void myFunction() {
+        int i = 10;
+        int j = 10 + i;
+        int y=i+j;
     }
 }
