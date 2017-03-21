d63f11543f68be11c9b6cdd999fff294fbd8e31e
anu-123 <r.annu492@gmail.com>
2017-03-21 18:43:29 +0530

    0001

--- app/src/main/java/com/example/hp/searchbar/jjk.java
+++ app/src/main/java/com/example/hp/searchbar/jjk.java
@@ -5,4 +5,10 @@ package com.example.hp.searchbar;
  */
 public class jjk {
   String msg="Checking branch one";
+  int temp=90+7;
+  int temp2=temp+10+30+55;
+  ///////////////commenting here
+  int y=90+0;
+  int f=77+55+y;
+  int tempy=temp+temp2;
 }
