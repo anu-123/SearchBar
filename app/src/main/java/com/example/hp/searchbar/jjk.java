29e0852f41bdd8aa48b1e62722c8f2c96e5da7de
anu-123 <r.annu492@gmail.com>
2017-03-21 18:49:42 +0530

    Merge branch 'Branch03' of https://github.com/anu-123/SearchBar into Branch03
    # Conflicts:
    #	app/src/main/java/com/example/hp/searchbar/jjk.java

--- app/src/main/java/com/example/hp/searchbar/jjk.java
+++ app/src/main/java/com/example/hp/searchbar/jjk.java
@@ -5,4 +5,11 @@ package com.example.hp.searchbar;
  */
 public class jjk {
   String msg="Checking branch one";
+   String msg2="Checking branch three";
+  int temp=90+7;
+  int temp2=temp+10+30+55;
+  ///////////////commenting here
+  int y=90+0;
+  int f=77+55+y;
+  int tempy=temp+temp2;
 }
